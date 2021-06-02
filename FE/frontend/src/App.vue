68b14fe2585fad95afea1777acb46ffba8911ff9
Ahmed Mahboub <ahmed.mahboub99@gmail.com>
2021-06-02 18:47:21 +0200

    feat(frontend app): :sparkles: test view routes
    edit app.vue to test views routes

--- FE/frontend/src/App.vue
+++ FE/frontend/src/App.vue
@@ -2,7 +2,12 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/contact">contact</router-link> |
+      <router-link to="/error">404</router-link> |
+      <router-link to="/profile">profile</router-link> |
+      <router-link to="/login">login</router-link> |
+      <router-link to="/signup">signup</router-link>
     </div>
     <router-view />
   </div>
