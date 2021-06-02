178c32059b4ae3d3aea95c539e44211145e6e9c8
Ahmed Mahboub <ahmed.mahboub99@gmail.com>
2021-06-02 18:46:07 +0200

    feat(router): :sparkles: add all main views' routes

--- FE/frontend/src/router/index.js
+++ FE/frontend/src/router/index.js
@@ -1,29 +1,56 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: () =>
+            import ('../views/Home.vue')
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: () =>
+            import ('../views/About.vue')
+    },
+    {
+        path: '/contact',
+        name: 'Contact',
+        component: () =>
+            import ('../views/ContactUs.vue')
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () =>
+            import ('../views/Login.vue')
+    }, {
+        path: '/error',
+        name: 'Error',
+        component: () =>
+            import ('../views/Page404.vue')
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        component: () =>
+            import ('../views/Profile.vue')
+    },
+    {
+        path: '/signup',
+        name: 'Signup',
+        component: () =>
+            import ('../views/Signup.vue')
+    },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
