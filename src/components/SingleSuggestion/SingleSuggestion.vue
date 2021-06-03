53761201667c5cc02aef62ff2b68ca76a7b25094
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2021-06-03 22:42:41 +0200

    feat: :sparkles: Add implementation of suggestions component

---
+++ src/components/SingleSuggestion/SingleSuggestion.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="suggestion">
+    <div class="suggestion__left">
+      <img
+        :src="user.userImg"
+        :alt="user.username"
+        class="suggestion__user-img"
+        draggable="false"
+      />
+
+      <div class="suggestion__info">
+        <div class="suggestion__username">{{ user.username }}</div>
+        <span class="suggestion__relation">{{ user.suggestionBase }}</span>
+      </div>
+    </div>
+
+    <div class="suggestion__right">
+      <a :href="'/' + user.id" class="suggestion__follow-link">Follow</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SingleSuggestion',
+  props: {
+    user: {
+      type: Object,
+      requires: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.suggestion {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 0.5rem;
+
+  &__left {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 6rem;
+    margin-right: 134px;
+  }
+
+  &__relation {
+    color: $dark-gray;
+    font-size: 80%;
+  }
+
+  &__user-img {
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+    margin-right: 1rem;
+  }
+
+  &__follow-link {
+    text-decoration: none;
+  }
+}
+</style>
---
+++ src/components/UserInfo/UserInfo.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="user-info">
+    <img
+      src="./../../assets/me.jpg"
+      alt="Hemdan's profile image"
+      class="user-info__img"
+      draggable="false"
+    />
+
+    <div class="user-names">
+      <div class="user-info__username">Abdallah Hemdan</div>
+      <span class="user-info__sub-name">Abdallah_Ahmed_Hemdan</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserInfo',
+};
+</script>
+
+<style lang="scss" scoped>
+.user-info {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  &__img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: transparent;
+    border: 2px solid $light-gray;
+    padding: 2px;
+    margin-right: 15px;
+  }
+
+  &__username {
+    font-size: 1rem;
+    font-weight: 600;
+  }
+
+  &__sub-name {
+    font-size: 0.8rem;
+    color: $dark-gray;
+  }
+}
+</style>
