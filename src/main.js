7d037b29a276e820a1815f4c5d3ecf78bf0fcc67
Ahmed Mahboub <ahmed.mahboub99@gmail.com>
2021-06-04 03:21:59 +0200

    feat: :sparkles: add fontwosome

--- src/main.js
+++ src/main.js
@@ -3,22 +3,30 @@ import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import firebase from 'firebase'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+library.add(faUserSecret)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 Vue.config.productionTip = false
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyB586VuDIIUbo5PPV7txZeGCLnV8jC_h4o',
-  authDomain: 'instahelp-482a0.firebaseapp.com',
-  projectId: 'instahelp-482a0',
-  storageBucket: 'instahelp-482a0.appspot.com',
-  messagingSenderId: '666106337917',
-  appId: '1:666106337917:web:4a58d47bd8d596d5b1af14',
-  measurementId: 'G-S77KTGH0NG',
-}
-// Initialize Firebase
+        apiKey: 'AIzaSyB586VuDIIUbo5PPV7txZeGCLnV8jC_h4o',
+        authDomain: 'instahelp-482a0.firebaseapp.com',
+        projectId: 'instahelp-482a0',
+        storageBucket: 'instahelp-482a0.appspot.com',
+        messagingSenderId: '666106337917',
+        appId: '1:666106337917:web:4a58d47bd8d596d5b1af14',
+        measurementId: 'G-S77KTGH0NG',
+    }
+    // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
 new Vue({
-  router,
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
